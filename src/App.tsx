7f97be4913181d9c55668712f7d2cd9c1e570099
Karim Essam <KaEssam17@gmail.com>
2026-06-06 17:00:54 +0300

    lesson: 03-children

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import './App.css';
+import { Card } from './Card';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Card>
+        <h3>Card Title</h3>
+        <p>Card Description</p>
+        <p>Card Data</p>
+      </Card>
+
+      <Card>
+        <h3>Karim</h3>
+        <p>karim@example.com</p>
+        <p>Card Data</p>
+      </Card>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Card.tsx
@@ -0,0 +1,4 @@
+
+export function Card({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
+}
