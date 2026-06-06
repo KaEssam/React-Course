96ec0eda302e467f18725c3bbe4df5ae3e417706
Karim Essam <KaEssam17@gmail.com>
2026-06-06 16:30:50 +0300

    lesson: 02-PROPS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import './App.css';
+import { Card } from './card';
 
 function App() {
-  return <></>;
+  // const nameData = 'Card Name';
+  // const descriptionData = 'Card Description';
+  // const data = 'test data';
+
+  const card1 = {
+    title: 'test 1',
+    description: 'test 1 description',
+    data: 'test 1 data',
+  };
+  return (
+    <>
+      {/* <Card title={nameData} description={descriptionData} data={data} /> */}
+      <Card cardObj={card1} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/card.tsx
@@ -0,0 +1,16 @@
+type props = {
+  title: string;
+  description: string;
+  data: string;
+};
+
+export function Card({ cardObj }: any) {
+  // export function Card({ title, description, data }: props) {
+  return (
+    <>
+      <h2>{cardObj.title}</h2>
+      <p>{cardObj.description}</p>
+      <p>{cardObj.data}</p>
+    </>
+  );
+}
