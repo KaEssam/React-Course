b37ac4b40e5e7fbbc61987c0a3a9ee0b19d7c299
Karim Essam <KaEssam17@gmail.com>
2026-06-06 15:40:09 +0300

    lesson: 01-JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import './App.css';
 
 function App() {
-  return <></>;
+  const name = 'React';
+  const age = 5;
+
+  function test() {
+    console.log('test');
+  }
+  return (
+    <>
+      <p className="hello" style={{ color: 'blue', fontSize: '50px' }}>
+        Hello, {name}, {age}
+      </p>
+      <p className="hello" style={{ color: 'blue', fontSize: '50px' }}>
+        Hello, {name}, {age}
+      </p>
+
+      <p className="hello" style={{ color: 'blue', fontSize: '50px' }}>
+        Hello, {name}, {age}
+      </p>
+
+      <button onClick={test}>Click me</button>
+    </>
+  );
 }
 
 export default App;
