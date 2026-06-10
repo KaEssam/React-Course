bc2ebef871e95002c5dc71ca9d4c78121be27e75
Karim Essam <KaEssam17@gmail.com>
2026-06-10 22:45:53 +0300

    lesson: 012-ActionState

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import './App.css';
 
+async function mockSave(title: string) {
+  await new Promise((r) => setTimeout(r, 1000));
+  if (!title) throw new Error('Title Required');
+}
+
+function SubmitBtn() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button type="submit" disabled={pending}>
+      {pending ? 'saving..' : 'save'}
+    </button>
+  );
+}
+
 function App() {
-  return <></>;
+  const [error, formAction] = useActionState(
+    async (v: string, formData: FormData) => {
+      try {
+        await mockSave(formData.get('name') as string);
+        return '';
+      } catch (e) {
+        return (e as Error).message;
+      }
+    },
+    '',
+  );
+  return (
+    <>
+      <form action={formAction}>
+        <input type="text" name="name" />
+        <SubmitBtn />
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </form>
+    </>
+  );
 }
 
 export default App;
