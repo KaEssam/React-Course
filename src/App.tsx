f0f1c3440e7a3c7f107a17c618fd277d22928d7b
Karim Essam <KaEssam17@gmail.com>
2026-06-10 21:10:46 +0300

    lesson: 09-useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useReducer } from 'react';
 import './App.css';
 
+type state = { count: number };
+type action = { type: 'inc' } | { type: 'dec' };
+
+function reducer(state: state, action: action) {
+  switch (action.type) {
+    case 'inc':
+      return { count: state.count + 1 };
+    case 'dec':
+      return { count: state.count - 1 };
+    default:
+      return { count: 0 };
+  }
+}
 function App() {
-  return <></>;
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  return (
+    <>
+      <p>Items: {state.count}</p>
+      <button onClick={() => dispatch({ type: 'inc' })}>+</button>
+      <button onClick={() => dispatch({ type: 'dec' })}>-</button>
+    </>
+  );
 }
 
 export default App;
