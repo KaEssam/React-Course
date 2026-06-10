c1f2bd64ff2f5693135352026f76e02ab61b1aa2
Karim Essam <KaEssam17@gmail.com>
2026-06-10 22:21:50 +0300

    lesson: 011-useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return <></>;
+  //   const [count, setCount] = useState(0);
+
+  //   useEffect(() => {
+  //     document.title = `clicked ${count}`;
+  //   }, [count]);
+
+  //   return (
+  //     <>
+  //       <button onClick={() => setCount((c) => c + 1)}>Count :{count}</button>
+  //     </>
+  //   );
+  // }
+
+  ////////////////////////////////////
+
+  const [sec, setSec] = useState(0);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setSec((s) => s + 1);
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, []);
+  return (
+    <>
+      <p>sec:{sec}</p>
+    </>
+  );
 }
 
 export default App;
