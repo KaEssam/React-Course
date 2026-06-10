90faf88d0db113ccd48683077328cfee6a8b8fb9
Karim Essam <KaEssam17@gmail.com>
2026-06-10 20:39:24 +0300

    lesson: 07-useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <></>;
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>+1</button>
+      <button onClick={() => setCount(0)}>reset</button>
+    </>
+  );
 }
 
 export default App;
