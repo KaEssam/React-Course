b4ff3a54f3591c66a9f2cf7742b3ab63d24948e2
Karim Essam <KaEssam17@gmail.com>
2026-06-10 21:34:07 +0300

    lesson: 010-lifting-state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import { useState } from 'react';
 import './App.css';
 
+const data = ['React', 'Next', 'Angular', 'NodeJs'];
+
+function SearchInput({
+  value,
+  onChange,
+}: {
+  value: string;
+  onChange: (v: string) => void;
+}) {
+  return <input value={value} onChange={(e) => onChange(e.target.value)} />;
+}
+
+function List({ items }: { items: string[] }) {
+  return (
+    <ul>
+      {items.map((i) => (
+        <li key={i}>{i}</li>
+      ))}
+    </ul>
+  );
+}
 function App() {
-  return <></>;
+  const [q, setQ] = useState('');
+  const filterList = data.filter((i) =>
+    i.toLowerCase().includes(q.toLowerCase()),
+  );
+  return (
+    <>
+      <SearchInput value={q} onChange={setQ} />
+      <List items={filterList} />
+    </>
+  );
 }
 
 export default App;
