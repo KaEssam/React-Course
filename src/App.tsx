e4b35defca20d6eabdbcb5fd13a53c4e75ae7489
Karim Essam <KaEssam17@gmail.com>
2026-06-10 20:50:39 +0300

    lesson: 08-state-batch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <></>;
+  const [count, setCount] = useState(0);
+
+  console.log('render', count);
+  function handelClick() {
+    setCount((c) => c + 1);
+    setCount((c) => c + 1);
+    setCount((c) => c + 1);
+  }
+
+  function handelLeftClick() {
+    setCount(count + 1);
+    setCount(count + 1);
+    setCount(count + 1);
+  }
+
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={handelClick}>+3</button>
+      <button onClick={handelLeftClick}>+3</button>
+    </>
+  );
 }
 
 export default App;
