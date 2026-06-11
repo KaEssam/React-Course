eeb08ad05795d352112ba8baafeed3a383268f0f
Karim Essam <KaEssam17@gmail.com>
2026-06-11 21:26:41 +0300

    lesson: 015-fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import { useToggle } from './useToggle';
 
+type post = { id: number; title: string };
 function App() {
-  const { on, toggle } = useToggle();
+  const [posts, setPosts] = useState<post[]>([]);
+
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/posts')
+  //     .then((r) => r.json())
+  //     .then(setPosts);
+  // }, []);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      signal: controller.signal,
+    })
+      .then((r) => r.json())
+      .then(setPosts)
+      .catch((e) => {
+        if (e.name !== 'AbortError') console.error(e);
+      });
+
+    return () => controller.abort();
+  }, []);
 
   return (
     <>
-      <button onClick={toggle}>{on ? 'Hide' : 'show'}</button>
-      {on && <p>Hi</p>}
-
-      <button onClick={toggle}>{on ? 'Hide' : 'show'}</button>
-      {on && <p>Hi</p>}
-
-      <button onClick={toggle}>{on ? 'Hide' : 'show'}</button>
-      {on && <p>Hi</p>}
+      <ul>
+        {posts.map((p) => (
+          <li key={p.id}>{p.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
--- src/useToggle.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-export function useToggle(init = false) {
-  const [on, setOn] = useState(init);
-  const toggle = () => setOn((v) => !v);
-
-  return { on, toggle } as const;
-}
