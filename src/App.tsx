cdfc77d4c1bb1f9103ff1ef19a7245e5bf1cbb0d
Karim Essam <KaEssam17@gmail.com>
2026-06-11 22:49:27 +0300

    018-tanStack-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
 import './App.css';
 
+type post = { id: number; title: string };
+
+async function fetchData(): Promise<post[]> {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  return res.json();
+}
 function App() {
-  return <></>;
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['posts'],
+    queryFn: fetchData,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>{(error as Error).message}</p>;
+  return (
+    <>
+      <ul>
+        {data!.map((p) => (
+          <li key={p.id}>{p.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
