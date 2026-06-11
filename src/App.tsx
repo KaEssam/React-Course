b0e119a299a04511ec42909b6a0a2a540cd9dab0
Karim Essam <KaEssam17@gmail.com>
2026-06-11 22:20:30 +0300

    lesson:  017-axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { api } from './client';
 
+type post = { id: number; title: string; body: string };
 function App() {
-  return <></>;
+  const [posts, setPosts] = useState<post[]>([]);
+
+  useEffect(() => {
+    api.get('/posts').then((res) => {
+      setPosts(res.data);
+    });
+  },[]);
+  return (
+    <>
+      <ul>
+        {posts.map((p) => (
+          <li key={p.id}>{p.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
---
+++ src/client.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response.status === 401) {
+      return Promise.reject(err);
+    }
+  },
+);
