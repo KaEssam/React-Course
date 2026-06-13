4dafa672b9885ba5d71c317b51bcbffe8bc4c1a0
Karim Essam <KaEssam17@gmail.com>
2026-06-13 17:28:47 +0300

    023-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import { useForm } from 'react-hook-form';
 import './App.css';
 
+type form = { email: string; pass: string };
 function App() {
-  return <></>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<form>();
+
+  function onSumbit(data: form) {
+    console.log('sumbited', data);
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSumbit)}>
+        <input
+          {...register('email', { required: 'email is required' })}
+          placeholder="email"
+        />
+        <p>{errors.email?.message}</p>
+        <input
+          {...register('pass', {
+            required: 'password is required',
+            minLength: {
+              value: 6,
+              message: 'password must be at least 6 characters',
+            },
+            maxLength: {
+              value: 20,
+              message: 'password must be less than 20 characters',
+            },
+          })}
+          placeholder="password"
+        />
+        <p>{errors.pass?.message}</p>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
 }
 
 export default App;
